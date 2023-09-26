9c42b3da710e72be2bafe091365bcaa6863bfb92
Vibhanshu Pandey <ivibhu05@gmail.com>
2023-09-26 12:18:33 +0530

    server setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,25 @@
 import express from "express";
+import "dotenv/config";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port:${PORT}`);
-});
+mongoose
+  .connect(
+    process.env.DATABASE_URL.replace(
+      "<password>",
+      process.env.DATABASE_PASSWORD
+    )
+  )
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port:${PORT}`);
+    });
+  })
+  .then(() => {
+    console.log("Mongo Connected!");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
